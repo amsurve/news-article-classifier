74e5a2c93c2fb9a845f648786dbb2e42855192b6
atharvasurve <atharva.m.surve@gmail.com>
2020-04-10 17:16:55 +0530

    added svc to training

--- model_train.py
+++ model_train.py
@@ -7,7 +7,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.svm import SVC
 import pickle
+from sklearn.metrics import classification_report
 
 # %%
 df = pd.read_csv('data/bbc_cleaned.csv')
@@ -40,21 +44,48 @@ y = df['label'].to_numpy()
 #Split Data 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
 #%%
+# For Support vector classifier
+parameters = {'C':[1, 10, 100],
+              'gamma':[0.1, 0.01]
+              }
+cv = GridSearchCV(SVC(), param_grid=parameters, cv = 3)
+
+cv.fit(X_train,y_train)
+
+y_pred = cv.predict(X_test)
+
+print("Accuracy: {}".format(cv.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+print("Tuned Model Parameters: {}".format(cv.best_params_))
+
+
+#%%
+# For Random forest
+rf_parameters = {'max_depth' : [int(x) for x in np.linspace(10, 50, num = 5)],
+                 'n_estimators' : [int(x) for x in np.linspace(start = 500, stop = 1000, num = 5)]
+              }
+
+cv = GridSearchCV(RandomForestClassifier(), param_grid=rf_parameters, cv = 3)
+
+cv.fit(X_train,y_train)
+
+y_pred = cv.predict(X_test)
+
+print("Accuracy: {}".format(cv.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+print("Tuned Model Parameters: {}".format(cv.best_params_))
+
+pickle.dump(cv, open("models/rfc.pkl", "wb"))
+
+#%%
+# For Logistic regression
 logc = LogisticRegression(random_state=42)
 logc.fit(X_train, y_train)
-# logc.score( X_test,y_test)
-# %%
-rfc = RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0)
-rfc.fit(X_train, y_train)
-rfc.score( X_test,y_test)
-
-# %%
 logc_scores = cross_val_score(logc, X, y, cv=5,scoring='accuracy')
 
-# %%
-rfc_scores = cross_val_score(rfc, X, y, cv=5,scoring='accuracy')
-print(rfc_scores.mean())
-#%%
-pickle.dump(rfc, open("models/rfc.pkl", "wb"))
-pickle.dump(logc, open("models/logc.pkl", "wb"))
+y_pred = logc.predict(X_test)
 
+print("Accuracy: {}".format(logc.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+
+pickle.dump(logc, open("models/logc.pkl", "wb"))
